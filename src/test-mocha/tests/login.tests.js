5fc6f0a3658f45d0b55ebba87cd0ac5ae5fa9874
Sergio E Bautista M <sebautistam@gmail.com>
2025-06-20 09:08:56 -0500

    Basic project structure set up for both Mocha and Cucumber

---
+++ src/test-mocha/tests/login.tests.js
@@ -0,0 +1,29 @@
+
+describe('Login page', () => {
+
+    //parametrize test data 
+    const testData = [
+        {name:"" , password:""}
+    ];
+
+    // open the website before each test
+    beforeEach( async() => {
+
+
+    });
+
+    testData.forEach( data => {
+        
+
+    });
+
+
+
+})
+
+
+/**
+ * @UC1 test login form with empty credentials
+ * @UC2 test login form with empty password
+ * @UC3 test login form with correct credentials
+ */
